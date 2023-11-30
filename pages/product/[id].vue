ff189593580d75fe1c060e886039e68680a3da2e
galsp <gs3260474@gmail.com>
2023-11-30 15:27:29 +0700

    modif id

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -1,29 +1,11 @@
 <script lang="ts" setup>
-import { useProductsStore } from "~/stores/products";
-
-const product = useProductsStore();
-const allProducts = ref([]);
-
-product.getAllProducts().then(() => {
-allProducts.value = product.products;
-});
-
-const selectedCategory = ref("");
-</script>
-
-definePageMeta({
-  middleware: ["user-access"]
- });
-
-
-<!-- <script lang="ts" setup>
 import { products } from "~/composables/constants/products";
 import { Products } from "~/types/products";
 const route = useRoute();
 const id =
   typeof route.params.id === "string" ? parseInt(route.params.id) : null;
 const product = ref(products.find((item: Products) => item.id === id));
-</script> -->
+</script>
 <template>
   <DetailProduct :product="product" />
 </template>
